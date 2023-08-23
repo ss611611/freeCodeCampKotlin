eb470c18eddc1157360470f6f927dd2ef59ecc17
ss611611 <ss611611@gmail.com>
2023-08-23 18:55:24 +0800

    Integer Type

---
+++ freeCodeCamp/src/main/kotlin/integerType/Main.kt
@@ -0,0 +1,13 @@
+package integerType
+
+fun main() {
+    var number = 22
+
+    val maxIntegerValue = Int.MAX_VALUE
+    val minIntegerValue = Int.MIN_VALUE
+
+    println("Int naximum value is: $maxIntegerValue")
+    println("Int minimum value is: $minIntegerValue")
+
+    number = 2147483647
+}
