869aee642b84e5bfe2f7ff23476aef14fdf0d9d8
ss611611 <ss611611@gmail.com>
2023-08-23 22:20:51 +0800

    Functions vararg keyword

--- freeCodeCamp/src/main/kotlin/functions/Main.kt
+++ freeCodeCamp/src/main/kotlin/functions/Main.kt
@@ -1,13 +1,18 @@
 package functions
 
 fun main(args: Array<String>) {
-    sendMessage(
-//        message = "Hello",
-        name = "Alexa"
-    )
+//    println( sum(5, 6, 7, 10,5,6,4,5,3,4,5,6,4,4,6,5))
+    sum(1,4,6,7,8,9,5,7,6)
 }
 
-fun sendMessage(name: String = "User", message: String = sendText()) {
-    println("Name = $name and message = $message")
+//fun sum(vararg numbers: Int): Int {
+//   var result = 0
+//    for (number in numbers){
+//        result += number
+//    }
+//
+//    return  result
+//}
+fun sum(vararg numbers: Int) {
+    numbers.forEach { println(it) }
 }
-fun sendText() = "Some text!"
