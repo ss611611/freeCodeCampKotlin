a1ac6ababa264659380cf9ad82b1befa591d31c8
ss611611 <ss611611@gmail.com>
2023-08-23 19:00:01 +0800

    Char and Boolean

---
+++ freeCodeCamp/src/main/kotlin/charAndBoolean/Main.kt
@@ -0,0 +1,8 @@
+package charAndBoolean
+
+fun main(args: Array<String>) {
+    val myCharValue = 'D'
+    val myBooleanValue= false
+
+    println("myCharValue = $myCharValue and myBolleanValue = $myBooleanValue")
+}
