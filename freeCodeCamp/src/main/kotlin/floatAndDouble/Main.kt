d8521bf0d9f84963f0e15c3fa46fdc1edc0dfb55
ss611611 <ss611611@gmail.com>
2023-08-23 18:58:55 +0800

    Float and Double

---
+++ freeCodeCamp/src/main/kotlin/floatAndDouble/Main.kt
@@ -0,0 +1,8 @@
+package floatAndDouble
+
+fun main(args: Array<String>) {
+    val myNumber = 2F
+    val mySecondNumber = 2.0
+
+    println("myNumber = $myNumber and mySecondNumber = $mySecondNumber")
+}
