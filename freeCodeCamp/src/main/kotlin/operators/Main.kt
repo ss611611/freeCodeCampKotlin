1688ae6ee79fa595a0dd93419545ba9e81333fc4
ss611611 <ss611611@gmail.com>
2023-08-23 19:04:19 +0800

    IfThenElse: Statement-Expression

--- freeCodeCamp/src/main/kotlin/operators/Main.kt
+++ freeCodeCamp/src/main/kotlin/operators/Main.kt
@@ -36,4 +36,13 @@ fun main(args: Array<String>) {
 
     println("x = ${x--}")
     println("x = ${--x}")
+
+    val myNumber = 150
+    if (myNumber >= 150) {
+        println("Greater than 150")
+    }else if (myNumber > 90){
+        println("Greater than 90")
+    }else {
+        println("All the conditions failed.")
+    }
 }
