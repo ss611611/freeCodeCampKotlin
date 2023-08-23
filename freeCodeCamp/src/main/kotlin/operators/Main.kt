23cede8e46f696affd47280a78ddcae3fafb7f7e
ss611611 <ss611611@gmail.com>
2023-08-23 19:08:31 +0800

    logical OR operator

--- freeCodeCamp/src/main/kotlin/operators/Main.kt
+++ freeCodeCamp/src/main/kotlin/operators/Main.kt
@@ -37,11 +37,15 @@ fun main(args: Array<String>) {
     println("x = ${x--}")
     println("x = ${--x}")
 
-    val isPlaying = true
-    val score = 100
-    if (isPlaying && score == 100){
-        println("Next level opened")
-    }else {
-        println("Still at the same level")
-    }
+//    val isPlaying = true
+//    val score = 100
+//    if (isPlaying && score == 100){
+//        println("Next level opened")
+//    }else {
+//        println("Still at the same level")
+//    }
+    val num1 = -5
+    val num2 = -3
+    if (num1 > 0 || num2 > 0) println("The condition is true") else println("The condition is false")
+    
 }
