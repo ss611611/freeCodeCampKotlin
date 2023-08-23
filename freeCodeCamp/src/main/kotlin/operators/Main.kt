3e14d613c07213e71ccfd1f3c17a683bb1013b27
ss611611 <ss611611@gmail.com>
2023-08-23 19:09:38 +0800

    When Statement-Expression

--- freeCodeCamp/src/main/kotlin/operators/Main.kt
+++ freeCodeCamp/src/main/kotlin/operators/Main.kt
@@ -47,5 +47,5 @@ fun main(args: Array<String>) {
     val num1 = -5
     val num2 = -3
     if (num1 > 0 || num2 > 0) println("The condition is true") else println("The condition is false")
-    
+
 }
