d525ef29057df1218ce5e88b03ba20eb06be0ab9
ss611611 <ss611611@gmail.com>
2023-08-23 18:57:25 +0800

    DataType: Byte, Short and Long

--- freeCodeCamp/src/main/kotlin/integerType/Main.kt
+++ freeCodeCamp/src/main/kotlin/integerType/Main.kt
@@ -10,4 +10,23 @@ fun main() {
     println("Int minimum value is: $minIntegerValue")
 
     number = 2147483647
+
+    val myMaxByteValue: Byte = Byte.MAX_VALUE
+    val myMinByteValue: Byte = Byte.MIN_VALUE
+
+    println("Byte naximum value is: $myMaxByteValue")
+    println("Byte minimum value is: $myMinByteValue")
+
+    val myMaxShortValue: Short = Short.MAX_VALUE
+    val myMinShortValue: Short = Short.MIN_VALUE
+
+    println("Short naximum value is: $myMaxShortValue")
+    println("Short minimum value is: $myMinShortValue")
+
+    val myMaxLongValue: Long = Long.MAX_VALUE
+    val myMinLongValue: Long = Long.MIN_VALUE
+
+    println("Long naximum value is: $myMaxLongValue")
+    println("Long minimum value is: $myMinLongValue")
+    val myNumber = 28
 }
