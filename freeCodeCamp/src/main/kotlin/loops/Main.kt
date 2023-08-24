2ae43ea52393a3daea6eb6d636a481459383b035
ss611611 <ss611611@gmail.com>
2023-08-24 22:48:55 +0800

    loops: WHILE and DO WHILE loop, Labels and continue and break

--- freeCodeCamp/src/main/kotlin/loops/Main.kt
+++ freeCodeCamp/src/main/kotlin/loops/Main.kt
@@ -1,22 +1,55 @@
 package loops
 
 fun main(args: Array<String>) {
-
-//    for (i in 1..10){
-//        println(i)
-//    }
-//    for (i in 1 until 10){
-//        println(i)
-//    }
-
-//    for (i in 10 downTo 1){
-//        println(i)
+//    var number = 10
+//    while (number < 10) println(number++)
+//    while (number < 10) println(++number)
+//    while (number < 10) {
+//        println(number)
+//        number++
 //    }
 
-//    for (i in 1 until 10 step 2){
+//    var number = 10
+//
+//    do {
+//        println(number)
+//    } while (number < 10)
+
+    var number = 0
+
+    outer@ while (number < 5) {
+        number++
+        println(number)
+
+        var i = 0
+
+        while (i < 5) {
+            if (i == 0) break@outer
+            i++
+            println("***%i")
+        }
+
+
+//        if (number in 3..7) {
+//            continue
+//        }
+
+//        if (number == 7) {
+//            break
+//        }
+//        println(number)
+//    }
+
+//    for (i in 0..10){
+//        if (i in 3..8){
+//            continue
+//        }
+
+//        for (i in 0..10){
+//            if (i == 7){
+//                break
+//            }
 //        println(i)
 //    }
-    for (i in 1..10){
-        println(i)
     }
 }
