6e47a7ea05fbfc565ad562b7a2bbe12e9e29c7a4
ss611611 <ss611611@gmail.com>
2023-08-26 22:34:20 +0800

    OOP: Classes

---
+++ freeCodeCamp/src/main/kotlin/oop/Mian.kt
@@ -0,0 +1,47 @@
+package oop
+
+fun main(args: Array<String>) {
+    val car1 = Car()
+    car1.name = "Tesla"
+    car1.model = "S Plaid"
+    car1.color = "Red"
+    car1.doors = 4
+
+    println("Name = ${car1.name} ")
+    println("Model = ${car1.model} ")
+    println("Color = ${car1.color} ")
+    println("Doors = ${car1.doors} ")
+
+    car1.move()
+    car1.stop()
+
+    val car2 = Car()
+    car2.name ="Ford"
+    car2.model = "Mustang"
+    car2.color = "Blue"
+    car2.doors = 2
+
+    println("\n")
+    println("Name = ${car2.name} ")
+    println("Model = ${car2.model} ")
+    println("Color = ${car2.color} ")
+    println("Doors = ${car2.doors} ")
+
+    car2.move()
+    car2.stop()
+}
+
+class Car {
+    var name = ""
+    var model = ""
+    var color = ""
+    var doors = 0
+
+    fun move() {
+        println("The car $name is moving")
+    }
+
+    fun stop() {
+        println("Thr car $name has stopped")
+    }
+}
