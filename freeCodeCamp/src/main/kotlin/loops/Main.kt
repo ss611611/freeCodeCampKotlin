69fd61b19e5b0e3a5ae37299a3e26768c9cfd7e7
ss611611 <ss611611@gmail.com>
2023-08-26 11:17:27 +0800

    loops Challenge

--- freeCodeCamp/src/main/kotlin/loops/Main.kt
+++ freeCodeCamp/src/main/kotlin/loops/Main.kt
@@ -1,6 +1,37 @@
 package loops
 
 fun main(args: Array<String>) {
+
+    var number = 1
+    val lastnumber = 20
+    var evenNumberCounter = 0
+
+    while (number <= lastnumber) {
+        number++
+
+        if ((number % 2) != 0) {
+            continue
+        }
+
+        evenNumberCounter++
+        println(number)
+    }
+
+    println("Now we are looping using the FOR loop")
+    evenNumberCounter = 0
+    for (i in 1..20) {
+        if ((i % 2) != 0) {
+            continue
+        }
+
+        evenNumberCounter++
+        println(i)
+    }
+    println("Total number of even numbers found = $evenNumberCounter")
+    println("Total number of even numbers found - using for loop -are: $evenNumberCounter")
+}
+
+
 //    var number = 10
 //    while (number < 10) println(number++)
 //    while (number < 10) println(++number)
@@ -15,20 +46,20 @@ fun main(args: Array<String>) {
 //        println(number)
 //    } while (number < 10)
 
-    var number = 0
-
-    outer@ while (number < 5) {
-        number++
-        println(number)
-
-        var i = 0
-
-        while (i < 5) {
-            if (i == 0) break@outer
-            i++
-            println("***%i")
-        }
-
+//    var number = 0
+//
+//    outer@ while (number < 5) {
+//        number++
+//        println(number)
+//
+//        var i = 0
+//
+//        while (i < 5) {
+//            if (i == 0) break@outer
+//            i++
+//            println("***%i")
+//        }
+//    }
 
 //        if (number in 3..7) {
 //            continue
@@ -51,5 +82,4 @@ fun main(args: Array<String>) {
 //            }
 //        println(i)
 //    }
-    }
-}
+
