a3b67a8e77cc5ea7083c6da193223e19d1d505b6
ss611611 <ss611611@gmail.com>
2023-08-27 15:29:47 +0800

    OOP: Enum Classes

--- freeCodeCamp/src/main/kotlin/oop/Mian.kt
+++ freeCodeCamp/src/main/kotlin/oop/Mian.kt
@@ -2,7 +2,27 @@ package oop
 
 
 fun main(args: Array<String>) {
+    val direction = Direction.valueOf("east".uppercase())
 
+    when(direction) {
+        Direction.EAST -> {
+            println("The direction is EAST")
+        }
+        Direction.WEST -> println("The direction is WEST")
+        Direction.NORTH -> println("The direction is NORTH")
+        Direction.SOUTH -> println("The direction is SOUTH")
+    }
+}
+
+enum class Direction(var direction: String, var distance: Int) {
+    NORTH("north",10),
+    SOUTH("south",20),
+    EAST("east",15),
+    WEST("west",40);
+
+    fun printData() {
+        println("Direction =$direction and Distance =$distance")
+    }
 }
 
 
