c38bde8bf60bf4c4ea417457994843a5602a4de3
ss611611 <ss611611@gmail.com>
2023-08-27 12:04:43 +0800

    OOP: lateinit keyword

--- freeCodeCamp/src/main/kotlin/oop/Mian.kt
+++ freeCodeCamp/src/main/kotlin/oop/Mian.kt
@@ -1,35 +1,45 @@
 package oop
 
-
 fun main(args: Array<String>) {
-    val user = User("Alex")
-    val friend = User(firstName = "John", lastName = "Smith")
-    val user2 = User(age = 19, firstName = "Ioana", lastName = "Dobbbi")
+    val user = User("Alex", "Dobbbi", 23)
 
-    user.firstName = "Vlad"
-
-    println(user.firstName)
-    println(user.lastName)
-    println(user.age)
-
-    println("\n")
-
-    println(friend.firstName)
-    println(friend.lastName)
-    println(friend.age)
-//    println("Name ${friend.name} ")
+    user.favoriteMovie = "Interstellar"
+    println(user.favoriteMovie)
 }
 
-class User(firstName: String, var lastName: String = "LastName", var age: Int = 0) {
-    var firstName = firstName
-        get() {
-            return "FirstName: $field"
-        }
-        set(value) {
-            println("$value was assigned to firstName property")
-            field = value
-        }
+class User(var firstName: String, var lastName: String, var age: Int) {
+    lateinit var favoriteMovie: String
 }
+
+//fun main(args: Array<String>) {
+//    val user = User("Alex")
+//    val friend = User(firstName = "John", lastName = "Smith")
+//    val user2 = User(age = 19, firstName = "Ioana", lastName = "Dobbbi")
+//
+//    user.firstName = "Vlad"
+//
+//    println(user.firstName)
+//    println(user.lastName)
+//    println(user.age)
+//
+//    println("\n")
+//
+//    println(friend.firstName)
+//    println(friend.lastName)
+//    println(friend.age)
+////    println("Name ${friend.name} ")
+//}
+
+//class User(firstName: String, var lastName: String = "LastName", var age: Int = 0) {
+//    var firstName = firstName
+//        get() {
+//            return "FirstName: $field"
+//        }
+//        set(value) {
+//            println("$value was assigned to firstName property")
+//            field = value
+//        }
+//}
 
 
 //class User(var name: String, var lastName: String, var age: Int){
