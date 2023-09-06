2c21309836332845b147e862b90ad04cc4316bfb
ss611611 <ss611611@gmail.com>
2023-09-06 21:58:32 +0800

    Flatten

--- freeCodeCamp/src/main/kotlin/collectionsOperations/Main.kt
+++ freeCodeCamp/src/main/kotlin/collectionsOperations/Main.kt
@@ -5,6 +5,23 @@ fun main() {
     println(numbers.associateWith { it.length })
     println(numbers.associateBy { it.first().uppercase() })
     println(numbers.associateBy(keySelector = {it.first().uppercase()}, valueTransform = {it.length}))
+
+    /*
+    Flatten
+     */
+//    val numbersSets = arrayOf(arrayOf(1,2,3,), arrayOf(4,5,6), arrayOf(7,8,9))
+//    println(numbersSets[2][2])
+    val numbersSets = listOf(setOf(1,2,3,), setOf(4,5,6), setOf(7,8,9))
+//    for (numbers in numbersSets) {
+//        for (number in numbers) {
+//            println(number)
+//        }
+//        println("\n")
+//    }
+    val numbersFlatten = numbersSets.flatten()
+    for (number in numbersFlatten){
+        println(number)
+    }
 }
 
 
