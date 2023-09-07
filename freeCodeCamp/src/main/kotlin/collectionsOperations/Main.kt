740793f6b17e35ae44b87feb751ef85dd7a26a2d
ss611611 <ss611611@gmail.com>
2023-09-07 22:03:57 +0800

    Aggregate Operations

--- freeCodeCamp/src/main/kotlin/collectionsOperations/Main.kt
+++ freeCodeCamp/src/main/kotlin/collectionsOperations/Main.kt
@@ -1,17 +1,31 @@
 package collectionsOperations
 
 fun main() {
-    val numbers = listOf("one", "two", "three", "four", "five")
-    println(numbers.elementAt(3))
-    println(numbers.first())
-    println(numbers.last())
-
-    println("\n")
-    println(numbers.first { it.length > 3 })
-    println(numbers.last { it.startsWith("f") })
-    println(numbers.random())
-    println(numbers.isEmpty())
+    val numbers = listOf(6, 10, 14, 4, 100)
+    println("The sum is ${numbers.sum()}")
+    println("The count is: ${numbers.count()}")
+    println("The average is: ${numbers.average()}")
+    println("The max value is: ${numbers.maxOrNull()}")
+    println("The nin value is: ${numbers.minOrNull()}")
+    println("The sum is: ${numbers.sumOf { it * 2 }}")
 }
+
+
+            /*
+            Retrieve Single Elements
+             */
+//fun main() {
+//    val numbers = listOf("one", "two", "three", "four", "five")
+//    println(numbers.elementAt(3))
+//    println(numbers.first())
+//    println(numbers.last())
+//
+//    println("\n")
+//    println(numbers.first { it.length > 3 })
+//    println(numbers.last { it.startsWith("f") })
+//    println(numbers.random())
+//    println(numbers.isEmpty())
+//}
 
 
 
