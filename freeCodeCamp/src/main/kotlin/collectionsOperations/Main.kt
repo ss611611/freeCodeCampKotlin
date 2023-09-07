efeddc4655c4bf12b260f5892d9223a1a1f409f4
ss611611 <ss611611@gmail.com>
2023-09-07 21:34:51 +0800

    Plus and Minus Operators

--- freeCodeCamp/src/main/kotlin/collectionsOperations/Main.kt
+++ freeCodeCamp/src/main/kotlin/collectionsOperations/Main.kt
@@ -1,33 +1,46 @@
 package collectionsOperations
 
 fun main() {
-    val  numbers = listOf("one","two","three","four")
-    val longThan3 = numbers.filter { it.length > 3 }
-    println(longThan3)
-
-    val numbersMap = mapOf("key 1" to 1, "key 2" to 2, "key 3" to 3, "key 101" to 101)
-    val filteredMap = numbersMap.filter { it.key.endsWith("1") && it.value > 100 }
-    println(filteredMap)
-
-    val filteredIdx = numbers.filterIndexed { index, value -> (index != 0) && (value.length < 5) }
-    val filteredNot = numbers.filterNot { it.length <= 3 }
-    println(filteredIdx)
-    println(filteredNot)
-
-    val mixedList = listOf(1,2,3,'A','B','C',"Hello World","Alex",false)
-    mixedList.filterIsInstance<Boolean>().forEach {
-        println(it)
-    }
-    //Partition
-    println("\n")
-
-    val (match,rest) = numbers.partition { it.length > 3 }
-    println(match)
-    println(rest)
-    println(numbers.any() {it.endsWith("e")})
-    println(numbers.none() {it.endsWith("w")})
-    println(numbers.all { it.length > 1 })
+    val numbers = mutableListOf("one","two","three","four")
+    val plusList = numbers + "five"
+    val minusList = numbers - mutableListOf("three","four")
+    println(plusList)
+    println(minusList)
 }
+
+
+     /*
+     Filtering, Test Predicates
+      */
+
+//fun main() {
+//    val  numbers = listOf("one","two","three","four")
+//    val longThan3 = numbers.filter { it.length > 3 }
+//    println(longThan3)
+//
+//    val numbersMap = mapOf("key 1" to 1, "key 2" to 2, "key 3" to 3, "key 101" to 101)
+//    val filteredMap = numbersMap.filter { it.key.endsWith("1") && it.value > 100 }
+//    println(filteredMap)
+//
+//    val filteredIdx = numbers.filterIndexed { index, value -> (index != 0) && (value.length < 5) }
+//    val filteredNot = numbers.filterNot { it.length <= 3 }
+//    println(filteredIdx)
+//    println(filteredNot)
+//
+//    val mixedList = listOf(1,2,3,'A','B','C',"Hello World","Alex",false)
+//    mixedList.filterIsInstance<Boolean>().forEach {
+//        println(it)
+//    }
+//    //Partition
+//    println("\n")
+//
+//    val (match,rest) = numbers.partition { it.length > 3 }
+//    println(match)
+//    println(rest)
+//    println(numbers.any() {it.endsWith("e")})
+//    println(numbers.none() {it.endsWith("w")})
+//    println(numbers.all { it.length > 1 })
+//}
 
 
 
