c95359fe08d3a49bb70317b7ac86e994a99242e4
ss611611 <ss611611@gmail.com>
2023-09-07 21:53:06 +0800

    Retrieve Collection parts

--- freeCodeCamp/src/main/kotlin/collectionsOperations/Main.kt
+++ freeCodeCamp/src/main/kotlin/collectionsOperations/Main.kt
@@ -1,10 +1,42 @@
 package collectionsOperations
 
 fun main() {
-    val numbers = mutableListOf("one","two","three","four","five")
-    println(numbers.groupBy { it.first().uppercase() })
-    println(numbers.groupBy(keySelector = {it.first()}, valueTransform = {it.uppercase()}))
+    val numbersStrings = mutableListOf("one", "two", "three", "four", "five","six")
+    println(numbersStrings.slice(1..3))
+    println(numbersStrings.slice(0..4 step 2))
+    println(numbersStrings.slice(setOf(3, 5, 0)))
+
+    println("\n")
+    println(numbersStrings.take(3))
+    println(numbersStrings.takeLast(3))
+    println(numbersStrings.drop(1))
+    println(numbersStrings.dropLast(5))
+
+    println("\n")
+    println(numbersStrings.takeWhile { !it.startsWith("f") })
+    println(numbersStrings.takeLastWhile { it != "three" })
+    println(numbersStrings.dropWhile { it.length == 3 })
+    println(numbersStrings.dropLastWhile { it.contains("i") })
+
+    val numbers = (0..13).toList()
+    println(numbers.chunked(3))
+    println(numbers.chunked(3) {it.sum()})
+
+    println("\n")
+    val numbersStrings2 = numbersStrings
+    println(numbersStrings2.windowed(3))
 }
+
+
+
+        /*
+        Grouping
+         */
+//fun main() {
+//    val numbers = mutableListOf("one","two","three","four","five")
+//    println(numbers.groupBy { it.first().uppercase() })
+//    println(numbers.groupBy(keySelector = {it.first()}, valueTransform = {it.uppercase()}))
+//}
 
 
         /*
